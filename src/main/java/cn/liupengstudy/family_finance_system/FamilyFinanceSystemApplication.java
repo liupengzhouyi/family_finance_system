391f46f3e45dd5b40ec65e322aa6dabc9889611e
liupeng <liupeng.0@outlook.com>
2019-11-12 11:05:33 +0800

    初始化 V 0.0.1

---
+++ src/main/java/cn/liupengstudy/family_finance_system/FamilyFinanceSystemApplication.java
@@ -0,0 +1,18 @@
+package cn.liupengstudy.family_finance_system;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+
+@SpringBootApplication
+@MapperScan("cn.liupeng.catering_management_system.mapper")
+@EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
+public class FamilyFinanceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyFinanceSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/liupengstudy/family_finance_system/FamilyFinanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liupengstudy.family_finance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyFinanceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
