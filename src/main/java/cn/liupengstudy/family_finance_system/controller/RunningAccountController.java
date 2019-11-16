24dc24654bf23715669a40ec5a2193c73c8a263e
liupeng <liupeng.0@outlook.com>
2019-11-16 09:24:30 +0800

    RunningAccount流水账的Controller V 0.0.9.3

---
+++ src/main/java/cn/liupengstudy/family_finance_system/controller/RunningAccountController.java
@@ -0,0 +1,125 @@
+package cn.liupengstudy.family_finance_system.controller;
+
+import cn.liupengstudy.family_finance_system.pojo.IntegerTpyeId;
+import cn.liupengstudy.family_finance_system.pojo.RunningAccount;
+import cn.liupengstudy.family_finance_system.service.Impl.ImplRunningAccountService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @文件名 cn.liupengstudy.family_finance_system.controller
+ * @描述
+ * @创建人 liupeng
+ * @作者联系方式 LIUPENG.0@outlook.com
+ * @创建时间 2019/11/16 - 9:04 上午
+ * @修改人和其它信息
+ */
+@RestController
+@RequestMapping(value = "/runningAccount")
+@Api(tags = "流水账控制器", description = "提供流水账的增删改查方法")
+public class RunningAccountController {
+
+    @Autowired
+    private ImplRunningAccountService implRunningAccountService;
+
+    /**
+     * @描述  获取流水账业务层操作类
+     * @参数  []
+     * @返回值  cn.liupengstudy.family_finance_system.service.Impl.ImplRunningAccountService
+     * @创建人  liupeng
+     * @作者联系方式 LIUPENG.0@outlook.com
+     * @创建时间  2019/11/16 - 9:14 上午
+     * @修改人和其它信息
+     */
+    @ApiOperation(value = "获取流水账业务层操作类")
+    public ImplRunningAccountService getImplRunningAccountService() {
+        return implRunningAccountService;
+    }
+
+    /**
+     * @描述  设置流水账业务层操作类
+     * @参数  [implRunningAccountService]
+     * @返回值  void
+     * @创建人  liupeng
+     * @作者联系方式 LIUPENG.0@outlook.com
+     * @创建时间  2019/11/16 - 9:14 上午
+     * @修改人和其它信息
+     */
+    @ApiOperation(value = "设置流水账业务层操作类")
+    public void setImplRunningAccountService(ImplRunningAccountService implRunningAccountService) {
+        this.implRunningAccountService = implRunningAccountService;
+    }
+
+    /**
+     * @描述  新增流水账
+     * @参数  []
+     * @返回值  java.lang.String
+     * @创建人  liupeng
+     * @作者联系方式 LIUPENG.0@outlook.com
+     * @创建时间  2019/11/16 - 9:14 上午
+     * @修改人和其它信息
+     */
+    @ApiOperation(value = "新增流水账")
+    @ResponseBody
+    @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public String addRunningAccount(@RequestBody RunningAccount runningAccount) {
+        int key = this.getImplRunningAccountService().insert(runningAccount);
+        String returnInformation = "error";
+        if (key == 1){
+            returnInformation = "success";
+        }
+        return returnInformation;
+    }
+
+    /**
+     * @描述  通过ID获取流水账
+     * @参数  [integerTpyeId]
+     * @返回值  cn.liupengstudy.family_finance_system.pojo.RunningAccount
+     * @创建人  liupeng
+     * @作者联系方式 LIUPENG.0@outlook.com
+     * @创建时间  2019/11/16 - 9:20 上午
+     * @修改人和其它信息
+     */
+    @ApiOperation(value = "通过ID获取流水账")
+    @ResponseBody
+    @RequestMapping(value = "/findByID", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public RunningAccount getRunningAccountByID(@RequestBody IntegerTpyeId integerTpyeId) {
+        return this.getImplRunningAccountService().selectByPrimaryKey(integerTpyeId.getId());
+    }
+
+    /**
+     * @描述  通过ID删除流水账
+     * @参数  [integerTpyeId]
+     * @返回值  java.lang.String
+     * @创建人  liupeng
+     * @作者联系方式 LIUPENG.0@outlook.com
+     * @创建时间  2019/11/16 - 9:21 上午
+     * @修改人和其它信息
+     */
+    @ApiOperation(value = "通过ID删除流水账")
+    @ResponseBody
+    @RequestMapping(value = "/deleteByID", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public String deleteRunningAccountByID(@RequestBody IntegerTpyeId integerTpyeId) {
+        int key = this.getImplRunningAccountService().deleteByPrimaryKey(integerTpyeId.getId());
+        String returnInformation = "error";
+        if (key == 1){
+            returnInformation = "success";
+        }
+        return returnInformation;
+    }
+
+    @ApiOperation(value = "更新流水账")
+    @ResponseBody
+    @RequestMapping(value = "/update", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public String updateRunningAccountByID(@RequestBody RunningAccount runningAccount) {
+        int key = this.getImplRunningAccountService().updateByPrimaryKey(runningAccount);
+        String returnInformation = "error";
+        if (key == 1){
+            returnInformation = "success";
+        }
+        return returnInformation;
+    }
+
+}
