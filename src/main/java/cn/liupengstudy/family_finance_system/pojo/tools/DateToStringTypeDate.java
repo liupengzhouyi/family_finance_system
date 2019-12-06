f0c931135b0065452bf1a20f1edbf22d47220374
liupeng <liupeng.0@outlook.com>
2019-12-06 23:25:32 +0800

    Date To String Type Date  V 0.0.20.1

---
+++ src/main/java/cn/liupengstudy/family_finance_system/pojo/tools/DateToStringTypeDate.java
@@ -0,0 +1,58 @@
+package cn.liupengstudy.family_finance_system.pojo.tools;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @文件名 cn.liupengstudy.family_finance_system.pojo.tools
+ * @描述
+ * @创建人 liupeng
+ * @作者联系方式 LIUPENG.0@outlook.com
+ * @创建时间 2019/12/6 - 11:17 下午
+ * @修改人和其它信息
+ */
+@ApiModel(value = "Date类型转String类型的Date数据")
+public class DateToStringTypeDate {
+
+    @ApiModelProperty(value = "String类型的Date数据")
+    private String stringTypeDate;
+
+    @ApiModelProperty(value = "Date类型的Date数据")
+    private Date date;
+
+    public DateToStringTypeDate() {
+    }
+
+    public DateToStringTypeDate(Date date) {
+        this.date = date;
+        this.setStringTypeDate();
+    }
+
+    public String getStringTypeDate() {
+        return stringTypeDate;
+    }
+
+    public void setStringTypeDate() {
+        Date date = this.getDate();
+        //format的格式可以任意
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            this.stringTypeDate = sdf.format(date);
+            System.out.println(this.stringTypeDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
